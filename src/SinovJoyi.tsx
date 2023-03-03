f25d47f3095ac3863c90b896d32e5d80177dab08
erkiinov <erkinovbavrom12@gmail.com>
2023-03-03 15:32:49 +0500

    pre

--- src/SinovJoyi.tsx
+++ src/SinovJoyi.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 function SinovJoyi() {
-  return <div>SinovJoyi</div>;
+  return (
+    <div>
+      SinovJoyi
+      <Link href="/components">Components</Link>
+    </div>
+  );
 }
 
 export default SinovJoyi;

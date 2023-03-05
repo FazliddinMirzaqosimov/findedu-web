46cc68d68a9d8b5c84ae86b83d30c02d46af5e33
erkiinov <erkinovbavrom12@gmail.com>
2023-03-05 22:23:06 +0500

    000

--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -13,192 +13,192 @@ import Carusel from "../components/carusel/carusel";
 import Breadcrumb from "@/components/Breadcrumb";
 import Button from "@/components/Button";
 const Components = () => {
-	return (
-		<div>
-			<Breadcrumb paths={[{ title: "O'quv Markazlari", link: "/" }]} />
-			<Space direction={"vertical"} size={[10, 150]}>
-				<Tag text={"tag"} color={"blue"} />
+  return (
+    <div>
+      <Breadcrumb paths={[{ title: "O'quv Markazlari", link: "/" }]} />
+      <Space direction={"vertical"} size={[10, 150]}>
+        <Tag text={"tag"} color={"blue"} />
 
-				<Input
-					dropdown={[
-						{
-							options: [
-								"Barchasi",
-								"Toshkent",
-								"Samarqand",
-								"Buxoro",
-								"Sirdaryo",
-								"Jizzax",
-								"Qashqadaryo",
-								"Andijon",
-								"Namangan",
-								"Farg’ona",
-								"Surxondaryo",
-								"Xorazm",
-								"Navoiy",
-								"Qoraqalpog’iston Respublikasi",
-							],
-							name: "Mintaqa",
-						},
-					]}
-					placeholder={"Kasb, fan yoki o’quv markaz nomini kiriting..."}
-					button={"Izlash"}
-				/>
-				<Input
-					dropdown={[
-						{
-							options: [
-								"Barchasi",
-								"Toshkent",
-								"Samarqand",
-								"Buxoro",
-								"Sirdaryo",
-								"Jizzax",
-								"Qashqadaryo",
-								"Andijon",
-								"Namangan",
-								"Farg’ona",
-								"Surxondaryo",
-								"Xorazm",
-								"Navoiy",
-								"Qoraqalpog’iston Respublikasi",
-							],
-							name: "Mintaqa",
-						},
-						{
-							options: [
-								"Barchasi",
-								"Toshkent",
-								"Samarqand",
-								"Buxoro",
-								"Sirdaryo",
-								"Jizzax",
-								"Qashqadaryo",
-								"Andijon",
-								"Namangan",
-								"Farg’ona",
-								"Surxondaryo",
-								"Xorazm",
-								"Navoiy",
-								"Qoraqalpog’iston Respublikasi",
-							],
-							name: "Darajasi",
-						},
-						{
-							options: [
-								"Barchasi",
-								"Toshkent",
-								"Samarqand",
-								"Buxoro",
-								"Sirdaryo",
-								"Jizzax",
-								"Qashqadaryo",
-								"Andijon",
-								"Namangan",
-								"Farg’ona",
-								"Surxondaryo",
-								"Xorazm",
-								"Navoiy",
-								"Qoraqalpog’iston Respublikasi",
-							],
-							name: "O'qish turi",
-						},
-						{
-							options: [
-								"Barchasi",
-								"Toshkent",
-								"Samarqand",
-								"Buxoro",
-								"Sirdaryo",
-								"Jizzax",
-								"Qashqadaryo",
-								"Andijon",
-								"Namangan",
-								"Farg’ona",
-								"Surxondaryo",
-								"Xorazm",
-								"Navoiy",
-								"Qoraqalpog’iston Respublikasi",
-							],
-							name: "Yonalish",
-						},
-						{
-							options: [
-								"Barchasi",
-								"Toshkent",
-								"Samarqand",
-								"Buxoro",
-								"Sirdaryo",
-								"Jizzax",
-								"Qashqadaryo",
-								"Andijon",
-								"Namangan",
-								"Farg’ona",
-								"Surxondaryo",
-								"Xorazm",
-								"Navoiy",
-								"Qoraqalpog’iston Respublikasi",
-							],
-							name: "Narxi",
-						},
-					]}
-					placeholder={"Kasb, fan yoki o’quv markaz nomini kiriting..."}
-				/>
+        <Input
+          dropdown={[
+            {
+              options: [
+                "Barchasi",
+                "Toshkent",
+                "Samarqand",
+                "Buxoro",
+                "Sirdaryo",
+                "Jizzax",
+                "Qashqadaryo",
+                "Andijon",
+                "Namangan",
+                "Farg’ona",
+                "Surxondaryo",
+                "Xorazm",
+                "Navoiy",
+                "Qoraqalpog’iston Respublikasi",
+              ],
+              name: "Mintaqa",
+            },
+          ]}
+          placeholder={"Kasb, fan yoki o’quv markaz nomini kiriting..."}
+          button={"Izlash"}
+        />
+        <Input
+          dropdown={[
+            {
+              options: [
+                "Barchasi",
+                "Toshkent",
+                "Samarqand",
+                "Buxoro",
+                "Sirdaryo",
+                "Jizzax",
+                "Qashqadaryo",
+                "Andijon",
+                "Namangan",
+                "Farg’ona",
+                "Surxondaryo",
+                "Xorazm",
+                "Navoiy",
+                "Qoraqalpog’iston Respublikasi",
+              ],
+              name: "Mintaqa",
+            },
+            {
+              options: [
+                "Barchasi",
+                "Toshkent",
+                "Samarqand",
+                "Buxoro",
+                "Sirdaryo",
+                "Jizzax",
+                "Qashqadaryo",
+                "Andijon",
+                "Namangan",
+                "Farg’ona",
+                "Surxondaryo",
+                "Xorazm",
+                "Navoiy",
+                "Qoraqalpog’iston Respublikasi",
+              ],
+              name: "Darajasi",
+            },
+            {
+              options: [
+                "Barchasi",
+                "Toshkent",
+                "Samarqand",
+                "Buxoro",
+                "Sirdaryo",
+                "Jizzax",
+                "Qashqadaryo",
+                "Andijon",
+                "Namangan",
+                "Farg’ona",
+                "Surxondaryo",
+                "Xorazm",
+                "Navoiy",
+                "Qoraqalpog’iston Respublikasi",
+              ],
+              name: "O'qish turi",
+            },
+            {
+              options: [
+                "Barchasi",
+                "Toshkent",
+                "Samarqand",
+                "Buxoro",
+                "Sirdaryo",
+                "Jizzax",
+                "Qashqadaryo",
+                "Andijon",
+                "Namangan",
+                "Farg’ona",
+                "Surxondaryo",
+                "Xorazm",
+                "Navoiy",
+                "Qoraqalpog’iston Respublikasi",
+              ],
+              name: "Yonalish",
+            },
+            {
+              options: [
+                "Barchasi",
+                "Toshkent",
+                "Samarqand",
+                "Buxoro",
+                "Sirdaryo",
+                "Jizzax",
+                "Qashqadaryo",
+                "Andijon",
+                "Namangan",
+                "Farg’ona",
+                "Surxondaryo",
+                "Xorazm",
+                "Navoiy",
+                "Qoraqalpog’iston Respublikasi",
+              ],
+              name: "Narxi",
+            },
+          ]}
+          placeholder={"Kasb, fan yoki o’quv markaz nomini kiriting..."}
+        />
 
-				<Counter
-					options={[
-						{ count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar" },
-						{ count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar" },
-						{ count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar" },
-					]}
-				/>
-				<ImageViewer />
-				<Courses />
-				<Carusel length={3} />
-				<div>
-					<Button label="Hello" border="full" type="primary" href="/home" />
-					<Button
-						label="Kirish"
-						border="full"
-						type="primary"
-						onClick={() => console.log("Hello")}
-					/>
-					<Button label="Ro'yhatdan o'tish" border="full" type="secondary" />
-					<Button
-						label="Ro'yhatdan o'tish"
-						border="half"
-						type="secondary"
-						width
-					/>
-				</div>
-				<Card
-					cardOptions={[
-						{
-							title: "Najot ta'lim",
-							description: "Biz ilm va tajriba ulashamiz",
-							score: 4.5,
-							imgUrl: { img1 },
-							href: "string",
-						},
-						{
-							title: "PDP It Company",
-							description: "Biz ilm va tajriba ulashamiz",
-							score: 3.5,
-							imgUrl: { img2 },
-							href: "string",
-						},
-						{
-							title: "Data learning center",
-							description: "Biz ilm va tajriba ulashamiz",
-							score: 5.5,
-							imgUrl: { img3 },
-							href: "string",
-						},
-					]}
-				/>
-			</Space>
-		</div>
-	);
+        <Counter
+          options={[
+            { count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar" },
+            { count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar" },
+            { count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar" },
+          ]}
+        />
+        <ImageViewer />
+        <Courses />
+        <Carusel length={3} />
+        <div>
+          <Button label="Hello" border="full" type="primary" href="/home" />
+          <Button
+            label="Kirish"
+            border="full"
+            type="primary"
+            onClick={() => console.log("Hello")}
+          />
+          <Button label="Ro'yhatdan o'tish" border="full" type="secondary" />
+          <Button
+            label="Ro'yhatdan o'tish"
+            border="half"
+            type="secondary"
+            width
+          />
+        </div>
+        <Card
+          cardOptions={[
+            {
+              title: "Najot ta'lim",
+              description: "Biz ilm va tajriba ulashamiz",
+              score: 4.5,
+              imgUrl: { img1 },
+              href: "string",
+            },
+            {
+              title: "PDP It Company",
+              description: "Biz ilm va tajriba ulashamiz",
+              score: 3.5,
+              imgUrl: { img2 },
+              href: "string",
+            },
+            {
+              title: "Data learning center",
+              description: "Biz ilm va tajriba ulashamiz",
+              score: 5.5,
+              imgUrl: { img3 },
+              href: "string",
+            },
+          ]}
+        />
+      </Space>
+    </div>
+  );
 };
 
 export default Components;

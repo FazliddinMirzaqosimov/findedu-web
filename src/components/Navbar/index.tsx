871dc885e95a2f1a1426cfbb45da21216b93cbbd
Abdujabbarof <abdujabborov6172@gmail.com>
2023-03-05 19:33:38 +0500

    new

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,6 +3,7 @@ import logo from  './logo.svg'
 import Image from 'next/image'
 import css from './navbar.module.scss'
 import Link from 'next/link'
+import Button from '../Button'
 
 function Navbar() {
   return (
@@ -14,13 +15,33 @@ function Navbar() {
 
             <nav>
                 <ul>
-                    <li><Link href={'/'}>O’quv markazlar</Link></li>
-                    <li><Link href={'/'}>Loyiha haqida</Link></li>
+                    <li><Link href={'#'}>O’quv markazlar</Link></li>
+                    <li><Link href={'#'}>Loyiha haqida</Link></li>
                 </ul>
+                <Button label='Kirish' type='primary' border='full' />
+                <Button label="Ro'yhatdan o'tish" type='secondary' border='full' />
             </nav>
         </div>
         <div className={css.line}>
-            <div className={css.dot}></div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="15"
+          height="15"
+          viewBox="0 0 9 9"
+          fill="none"
+          className={css.dot}
+        >
+          <path
+            d="M9 4.5C9 6.98528 6.98528 9 4.5 9C2.01472 9 0 6.98528 0 4.5C0 2.01472 2.01472 0 4.5 0C6.98528 0 9 2.01472 9 4.5Z"
+            fill="#461773"
+          />
+          <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M2.39411 2.52192C2.86148 2.05639 3.44784 1.8005 3.88867 1.61508C4.0523 1.54625 4.2432 1.61997 4.31615 1.7818C4.3891 1.94364 4.31608 2.1315 4.15164 2.19839C3.76643 2.35506 3.25341 2.57336 2.84777 2.97739C2.39775 3.42563 2.10511 4.01688 1.96737 4.65748C1.93005 4.83103 1.76972 4.95506 1.59376 4.93156C1.41781 4.90807 1.29305 4.746 1.32873 4.5721C1.49664 3.75364 1.87758 3.03641 2.39411 2.52192Z"
+            fill="white"
+          />
+        </svg>
         </div>
     </header>
   )
--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -3,24 +3,35 @@ import Input from "@/components/Input/Input";
 import React from "react";
 import Tag from "@/components/Tag/tag";
 import style from '@/styles/components.module.scss'
+import Button from "@/components/Button";
 import {Space} from "antd";
+import Layout from "@/components/Layout";
 
 const Components = () => {
     return (
         <div>
-            <Space direction={"vertical"} size={[10,150]}>
-                <Input/>
-                <Tag text={'tag'} color={'blue'}/>
+            <Layout>
+                <Space direction={"vertical"} size={[10,150]}>
+                    <Input/>
+                    <Tag text={'tag'} color={'blue'}/>
 
-                <Counter
-                    options={[
-                        {count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar"},
-                        {count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar"},
-                        {count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar"},
-                    ]}
-                />
+                    <Counter
+                        options={[
+                            {count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar"},
+                            {count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar"},
+                            {count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar"},
+                        ]}
+                    />
 
-            </Space>
+                    <div>
+                        <Button label='Chiqish' type='primary' border='full' href="/" />
+                        <Button label="Ro'yhatdan o'tish" type='secondary' border='full' />
+                        <Button label="Ro'yhatdan o'tish" type='primary' border='half' width />
+                        <Button label="Ro'yhatdan o'tish" type='secondary' border='full' disable />
+                        <Button label="Ro'yhatdan o'tish" type='primary' border='full' disable />
+                    </div>
+                </Space>
+            </Layout>
         </div>
     );
 };

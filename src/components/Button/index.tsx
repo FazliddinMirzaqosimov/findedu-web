5ac938543d6c0eaad2fcea0b44150611c0d48fc5
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-03-05 21:03:51 +0500

    cards have been added

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,26 +1,53 @@
-import Link from 'next/link'
-import React from 'react'
-import css from './button.module.scss'
+import Link from "next/link";
+import css from "./button.module.scss";
 
 interface ButtonProps {
-  label: string
-  disable?: boolean
-  type: 'primary' | 'secondary'
-  border: 'full' | 'half'
-  width?: boolean
-  href?: string;
-  onClick?: (React.MouseEventHandler<HTMLAnchorElement> & React.MouseEventHandler<HTMLButtonElement>) | undefined;
+	label: string;
+	disable?: boolean;
+	type: "primary" | "secondary";
+	border: "full" | "half";
+	width?: boolean;
+	href?: string;
+	onClick?:
+		| (React.MouseEventHandler<HTMLAnchorElement> &
+				React.MouseEventHandler<HTMLButtonElement>)
+		| undefined;
 }
 
-function Button({label, disable, type, border, width, onClick, href}: ButtonProps) {
-  
-  if(href){
-    return <Link href={href} className={`${css.btn} ${type === 'primary' ? css.btnPrimary : css.btnSecondary} ${border === 'full' ? css.regular : css.half} ${width && css.width}`} onClick={onClick}>{label}</Link>
-  }
+function Button({
+	label,
+	disable,
+	type,
+	border,
+	width,
+	onClick,
+	href,
+}: ButtonProps) {
+	if (href) {
+		return (
+			<Link
+				href={href}
+				className={`${css.btn} ${
+					type === "primary" ? css.btnPrimary : css.btnSecondary
+				} ${border === "full" ? css.regular : css.half} ${width && css.width}`}
+				onClick={onClick}
+			>
+				{label}
+			</Link>
+		);
+	}
 
-  return (
-    <button className={`${css.btn} ${type === 'primary' ? css.btnPrimary : css.btnSecondary} ${border === 'full' ? css.regular : css.half} ${width && css.width}`} disabled={disable ? true : false} onClick={onClick}>{label}</button>
-  )
+	return (
+		<button
+			className={`${css.btn} ${
+				type === "primary" ? css.btnPrimary : css.btnSecondary
+			} ${border === "full" ? css.regular : css.half} ${width && css.width}`}
+			disabled={disable ? true : false}
+			onClick={onClick}
+		>
+			{label}
+		</button>
+	);
 }
 
-export default Button
+export default Button;
--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -2,27 +2,60 @@ import Counter from "@/components/counter/Counter";
 import Input from "@/components/Input/Input";
 import React from "react";
 import Tag from "@/components/Tag/tag";
-import style from '@/styles/components.module.scss'
-import {Space} from "antd";
-
+import style from "@/styles/components.module.scss";
+import { Space } from "antd";
+import Card from "@/components/card";
+import img1 from "../assets/media/Najot_Talim-01.png";
+import img2 from "../assets/media/Najot_Talim-01.png";
+import img3 from "../assets/media/Najot_Talim-01.png";
 const Components = () => {
-    return (
-        <div>
-            <Space direction={"vertical"} size={[10,150]}>
-                <Input/>
-                <Tag text={'tag'} color={'blue'}/>
+	return (
+		<div>
+			<Space direction={"vertical"} size={[10, 150]}>
+				<Input />
+				<Tag text={"tag"} color={"blue"} />
 
-                <Counter
-                    options={[
-                        {count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar"},
-                        {count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar"},
-                        {count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar"},
-                    ]}
-                />
-
-            </Space>
-        </div>
-    );
+				<Counter
+					options={[
+						{ count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar" },
+						{ count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar" },
+						{ count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar" },
+					]}
+				/>
+				<Card
+					cardOptions={[
+						// {
+						// 	title: "Najot ta'lim",
+						// 	description: "Biz ilm va tajriba ulashamiz",
+						// 	score: 4.5,
+						// 	imgUrl: {img1},
+						// },
+						{
+							title: "Najot ta'lim",
+							description: "Biz ilm va tajriba ulashamiz",
+							score: 4.5,
+							imgUrl: {img1},
+							href: "string", 
+						},
+						{
+							title: "PDP It Company",
+							description: "Biz ilm va tajriba ulashamiz",
+							score: 3.5,
+							imgUrl: {img2},
+							href: "string",
+						},
+						{
+							title: "Data learning center",
+							description: "Biz ilm va tajriba ulashamiz",
+							score: 5.5,
+							imgUrl: {img3},
+							href: "string",
+						},
+					]}
+				/>
+			</Space>
+		</div>
+	);
 };
 
 export default Components;

b3cdde3daa2386c3101814de5ac0af7302ef54d3
erkiinov <erkinovbavrom12@gmail.com>
2023-03-05 21:21:25 +0500

    s

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,73 +2,44 @@ import Link from "next/link";
 import css from "./button.module.scss";
 
 interface ButtonProps {
-<<<<<<< HEAD
-	label: string;
-	disable?: boolean;
-	type: "primary" | "secondary";
-	border: "full" | "half";
-	width?: boolean;
-	href?: string;
-	onClick?:
-		| (React.MouseEventHandler<HTMLAnchorElement> &
-				React.MouseEventHandler<HTMLButtonElement>)
-		| undefined;
+  label: string;
+  disable?: boolean;
+  type: "primary" | "secondary";
+  border: "full" | "half";
+  width?: boolean;
+  href?: string;
+  onClick?:
+    | (React.MouseEventHandler<HTMLAnchorElement> &
+        React.MouseEventHandler<HTMLButtonElement>)
+    | undefined;
 }
 
 function Button({
-	label,
-	disable,
-	type,
-	border,
-	width,
-	onClick,
-	href,
+  label,
+  disable,
+  type,
+  border,
+  width,
+  href,
+  onClick,
 }: ButtonProps) {
-	if (href) {
-		return (
-			<Link
-				href={href}
-				className={`${css.btn} ${
-					type === "primary" ? css.btnPrimary : css.btnSecondary
-				} ${border === "full" ? css.regular : css.half} ${width && css.width}`}
-				onClick={onClick}
-			>
-				{label}
-			</Link>
-		);
-	}
+  const MyButton = (
+    <button
+      className={`${css.btn} ${
+        type === "primary" ? css.btnPrimary : css.btnSecondary
+      } ${border === "full" ? css.regular : css.half} ${width && css.width}`}
+      disabled={disable ? true : false}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
 
-	return (
-		<button
-			className={`${css.btn} ${
-				type === "primary" ? css.btnPrimary : css.btnSecondary
-			} ${border === "full" ? css.regular : css.half} ${width && css.width}`}
-			disabled={disable ? true : false}
-			onClick={onClick}
-		>
-			{label}
-		</button>
-	);
-=======
-  label: string;
-  disable?: boolean;
-  type: 'primary' | 'secondary';
-  border: 'full' | 'half';
-  width?: boolean;
-  href?: string;
-  onClick?: (React.MouseEventHandler<HTMLAnchorElement> & React.MouseEventHandler<HTMLButtonElement>) | undefined;
-}
-
-function Button({label, disable, type, border, width, href ,onClick}: ButtonProps) {
-
-  const MyButton = <button className={`${css.btn} ${type === 'primary' ? css.btnPrimary : css.btnSecondary} ${border === 'full' ? css.regular : css.half} ${width && css.width}`} disabled={disable ? true : false} onClick={onClick}>{label}</button>
-
-  if(href) {
-    return <Link href={href}>{MyButton}</Link>
+  if (href) {
+    return <Link href={href}>{MyButton}</Link>;
   }
 
-  return MyButton
->>>>>>> 23177bab9697560594b2678ae3a36b3d064288fb
+  return MyButton;
 }
 
 export default Button;
--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -4,60 +4,6 @@ import React from "react";
 import Tag from "@/components/Tag/tag";
 import style from "@/styles/components.module.scss";
 import { Space } from "antd";
-<<<<<<< HEAD
-import Card from "@/components/card";
-import img1 from "../assets/media/Najot_Talim-01.png";
-import img2 from "../assets/media/Najot_Talim-01.png";
-import img3 from "../assets/media/Najot_Talim-01.png";
-const Components = () => {
-	return (
-		<div>
-			<Space direction={"vertical"} size={[10, 150]}>
-				<Input />
-				<Tag text={"tag"} color={"blue"} />
-
-				<Counter
-					options={[
-						{ count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar" },
-						{ count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar" },
-						{ count: 345, label: "Ro’yhatdan o’tkan o’quv markazlar" },
-					]}
-				/>
-				<Card
-					cardOptions={[
-						// {
-						// 	title: "Najot ta'lim",
-						// 	description: "Biz ilm va tajriba ulashamiz",
-						// 	score: 4.5,
-						// 	imgUrl: {img1},
-						// },
-						{
-							title: "Najot ta'lim",
-							description: "Biz ilm va tajriba ulashamiz",
-							score: 4.5,
-							imgUrl: {img1},
-							href: "string", 
-						},
-						{
-							title: "PDP It Company",
-							description: "Biz ilm va tajriba ulashamiz",
-							score: 3.5,
-							imgUrl: {img2},
-							href: "string",
-						},
-						{
-							title: "Data learning center",
-							description: "Biz ilm va tajriba ulashamiz",
-							score: 5.5,
-							imgUrl: {img3},
-							href: "string",
-						},
-					]}
-				/>
-			</Space>
-		</div>
-	);
-=======
 import ImageViewer from "@/components/ImageViewer/ImageViewer";
 import Courses from "@/components/courses/Courses";
 import Carusel from "../components/carusel/carusel";
@@ -225,7 +171,6 @@ const Components = () => {
       </Space>
     </div>
   );
->>>>>>> 23177bab9697560594b2678ae3a36b3d064288fb
 };
 
 export default Components;

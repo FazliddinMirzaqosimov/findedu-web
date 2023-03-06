9fd21f11ff5b3a436fe1435c4d674aa6be6a4509
Karimov Kamoliddin <karimovkamaladdin@gmail.com>
2023-03-06 22:28:41 +0500

    ..

--- src/pages/components.tsx
+++ src/pages/components.tsx
@@ -18,6 +18,7 @@ import Navbar from "@/components/Navbar";
 import Sponsor from "@/components/Sponsor";
 import DynamicList from "@/components/Dynamic section/dynamic-list";
 import { ImagesData, ImagesData2 } from "@/components/ImageViewer/imagesData/imagesdata";
+import { CoursesData } from "@/components/courses/coursesData/coursesData";
 
 const DynamicSectionArr = [
   {
@@ -232,7 +233,7 @@ const Components = () => {
         <ImageViewer data={ImagesData2}/>
 		<ImageViewer data={ImagesData} />
 		<ImageViewer width={1300} height={600} src='https://c4.wallpaperflare.com/wallpaper/500/442/354/outrun-vaporwave-hd-wallpaper-thumb.jpg' />
-        <Courses />
+        <Courses data={CoursesData} />
         <Carusel length={3} />
         <div>
           <Button label="Hello" border="full" type="primary" href="/home" />

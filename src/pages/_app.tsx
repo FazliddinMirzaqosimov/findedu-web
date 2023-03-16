d24b66f590d1ee9e2422f42a7f8812c9dbf794b8
Islom0906 <iabdugofurov99@gmail.com>
2023-03-16 22:47:35 +0500

    layoout in _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import {Provider} from "react-redux";
 import store from "@/service/redux/store";
 import {QueryClient, QueryClientProvider} from "react-query";
 import {ReactQueryDevtools} from "react-query/devtools";
+import Layout from "@/components/Layout";
 
 const queryClient = new QueryClient()
 export default function App({Component, pageProps}: AppProps) {
@@ -12,7 +13,9 @@ export default function App({Component, pageProps}: AppProps) {
         <>
             <Provider store={store}>
                 <QueryClientProvider client={queryClient}>
+                    <Layout>
                     <Component {...pageProps} />
+                    </Layout>
                     <ReactQueryDevtools initialIsOpen={false} position={'bottom-right'}/>
                 </QueryClientProvider>
             </Provider>

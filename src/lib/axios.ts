8b9825b5c58ba515d0c30c8c932f2e619eaea926
Islom0906 <iabdugofurov99@gmail.com>
2023-03-26 00:41:56 +0500

    register post tayor

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,15 +2,12 @@ import { getItem } from "./../service/helpers/persistance-storage";
 import axios, { AxiosInstance } from "axios";
 
 axios.defaults.baseURL = "http://3.138.61.64";
-//  const instance:AxiosInstance=axios.create({
-//     baseURL:'http://18.221.130.228'
-// })
 
 axios.interceptors.request.use((config) => {
   const token = getItem("token");
   console.log(config.headers);
 
-  const authorization = token ? `Bareer ${token}` : "";
+  const authorization = token ? `Bearer ${token}` : "";
   config.headers.Authorization = authorization;
   return config;
 });
